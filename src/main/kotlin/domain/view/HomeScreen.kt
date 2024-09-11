df1dafb0a35eb5d39002a496b80e4a83cdb3dece
lolmageap <ekxk1234@naver.com>
2024-09-11 22:07:31 +0900

    add home screen

---
+++ src/main/kotlin/domain/view/HomeScreen.kt
@@ -0,0 +1,25 @@
+package domain.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun homeScreen() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to Home Page!", style = MaterialTheme.typography.h4)
+        }
+    }
+}
