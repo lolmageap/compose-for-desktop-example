af3134459c3d97c1eee198b51bcec3ac32670b86
lolmageap <ekxk1234@naver.com>
2024-09-13 19:14:12 +0900

    create top app bar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
@@ -10,6 +15,7 @@ import androidx.compose.ui.window.application
 import domain.view.Screen.*
 import domain.view.homeScreen
 import domain.view.loginScreen
+import domain.view.createPostScreen
 import domain.view.signUpScreen
 
 fun main() = application {
@@ -23,17 +29,49 @@ fun main() = application {
         ),
     ) {
         var currentScreen by remember { mutableStateOf(LOGIN) }
+        val showTopAppBar = currentScreen != LOGIN && currentScreen != SIGN_UP
+        MaterialTheme {
+            Scaffold(
+                topBar = {
+                    if (showTopAppBar) {
+                        TopAppBar(
+                            title = { Text("Baum Blog") },
+                            modifier = Modifier.clickable {
+                                currentScreen = HOME
+                            },
+                            actions = {
+                                TextButton(onClick = { currentScreen = CREATE_POST }) {
+                                    Text("게시글 등록", color = MaterialTheme.colors.onPrimary)
+                                }
 
-        when (currentScreen) {
-            LOGIN -> loginScreen(
-                onLoginSuccess = { currentScreen = HOME },
-                moveToSignUp = { currentScreen = SIGN_UP },
+                                Spacer(modifier = Modifier.width(8.dp))
+
+                                TextButton(onClick = {
+                                    currentScreen = LOGIN
+                                }) {
+                                    Text("로그아웃", color = MaterialTheme.colors.onPrimary)
+                                }
+                            }
+                        )
+                    }
+                },
+                content = {
+                    when (currentScreen) {
+                        LOGIN -> loginScreen(
+                            onLoginSuccess = { currentScreen = HOME },
+                            moveToSignUp = { currentScreen = SIGN_UP },
+                        )
+
+                        SIGN_UP -> signUpScreen(
+                            onSignUpSuccess = { currentScreen = HOME },
+                            backToLogin = { currentScreen = LOGIN },
+                        )
+
+                        HOME -> homeScreen()
+                        CREATE_POST -> createPostScreen()
+                    }
+                }
             )
-            SIGN_UP -> signUpScreen(
-                onSignUpSuccess = { currentScreen = HOME },
-                backToLogin = { currentScreen = LOGIN },
-            )
-            HOME -> homeScreen()
         }
     }
 }
