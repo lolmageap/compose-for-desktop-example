80d3114ad4a0d1ab158fcc1e44f73df1747ce21e
lolmageap <ekxk1234@naver.com>
2024-09-19 22:18:01 +0900

    add logout function

--- src/main/kotlin/domain/component/Storage.kt
+++ src/main/kotlin/domain/component/Storage.kt
@@ -3,9 +3,14 @@ package domain.component
 import java.util.prefs.Preferences
 
 fun getTokenFromPreferences(): String? =
-    Preferences.userRoot().get("auth_token", null)
+    Preferences.userRoot().get(AUTH_TOKEN, null)
 
 fun saveTokenToPreferences(
     token: String,
 ) =
-    Preferences.userRoot().put("auth_token", token)
+    Preferences.userRoot().put(AUTH_TOKEN, token)
+
+fun logout() =
+    Preferences.userRoot().remove(AUTH_TOKEN)
+
+private const val AUTH_TOKEN = "auth_token"
